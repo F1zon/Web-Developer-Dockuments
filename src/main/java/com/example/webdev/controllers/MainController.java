2c0d8fda249eda719071344afa94de04a9e967b6
nikita <nik_iv02@mail.ru>
2024-07-22 08:22:10 +0300

    delete proj front

--- src/main/java/com/example/webdev/controllers/MainController.java
+++ src/main/java/com/example/webdev/controllers/MainController.java
@@ -5,13 +5,11 @@ import com.example.webdev.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
 @RestController
 public class MainController {
 
